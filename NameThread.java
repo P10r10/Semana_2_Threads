6f0d6e32d5584b3de73fb876eb0e0c4d112017f6
P10r10 <asantiago@keemail.me>
2023-09-18 16:16:28 +0100

    1.1

--- NameThread.java
+++ NameThread.java
@@ -3,15 +3,27 @@ import java.util.Random;
 class NameThread extends Thread {
 
     private int id;
-    private int sleepDuration;
 
     public NameThread(int id) {
         this.id = id;
-        sleepDuration = new Random().nextInt(1001) + 1000;
     }
 
     @Override
     public void run() {
-        System.out.println("Id: " + id);
+        int sleepDuration = new Random().nextInt(1001) + 1000;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Id: " + id);
+            try {
+                Thread.sleep(sleepDuration);
+            } catch (Exception e) {
+                System.out.println("We will deal with this later!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new NameThread(1).run();
+        new NameThread(2).run();
     }
 }
